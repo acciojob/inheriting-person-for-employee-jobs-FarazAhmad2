8b403e6b241813f122a3a919be303ec0940f57f2
Faraz Ahmad <102180103+FarazAhmad2@users.noreply.github.com>
2024-05-13 22:30:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,25 +1,26 @@
 // complete this js code
-class Person {
-  constructor(name, age) {
-    this.name = name;
-    this.age = age;
-  }
-
-  greet() {
-    return `Hello, my name is ${this.name}, I am ${this.age} years old.`;
-  }
+function Person(name, age) {
+  this.name = name;
+  this.age = age;
 }
 
-class Employee extends Person {
-  constructor(name, age, jobTitle) {
-    super(name, age);
-    this.jobTitle = jobTitle;
-  }
-
-  jobGreet() {
-    return `Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`;
-  }
+Person.prototype.greet = function () {
+  return `Hello, my name is ${this.name}, I am ${this.age} years old.`;
 }
+
+function Employee(name, age, jobTitle) {
+  this.name = name;
+  this.age = age;
+  this.jobTitle = jobTitle;
+}
+
+Employee.prototype = Object.create(Person.prototype);
+Employee.prototype.constructor = Employee;
+
+Employee.prototype.jobGreet = function () {
+  return `Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`
+}
+
 
 // Do not change code below this line
 window.Person = Person;
