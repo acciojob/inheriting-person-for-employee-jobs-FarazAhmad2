2cd4905ba1ba64616802bb5423eef2666c399395
Faraz Ahmad <102180103+FarazAhmad2@users.noreply.github.com>
2024-05-13 22:41:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -20,6 +20,12 @@ Employee.prototype.jobGreet = function () {
   return `Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`
 }
 
+et alice = new Person("Alice", 25);
+console.log(alice.greet()); // This should log: Hello, my name is Alice, I am 25 years old.
+
+let bob = new Employee("Bob", 30, "Manager");
+console.log(bob.greet()); // This should log: Hello, my name is Bob, I am 30 years old.
+console.log(bob.jobGreet()); // This should log: Hello, my name is Bob, I am 30 years old, and my job title is Manager.
 
 // Do not change code below this line
 window.Person = Person;
